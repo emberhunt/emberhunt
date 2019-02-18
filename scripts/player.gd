93d46c32a3291eddeb878f1390333a3c35ef486a
Ícaro Rodrigues Peixoto <icaropeixoto7@gmail.com>
2019-02-18 15:47:43 +0000

    Fixed player movement, added player stats

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,14 @@
-extends KinematicBody2D
+extends Node2D
 
-var speed = 0
-var direction = 0
-var motion = Vector2(0,0)
-var player_sprite = get_parent().get_node("player_sprite")
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var motion = direction*speed*delta*Vector2(100, 100)
-	move_and_slide(motion)
-	if direction != 0:
-		player_sprite.scale = ceil(direction)
-	
+var playerStats = { #Player Stats dictionary
+	"healthCurrent": 0,
+	"healthMax": 100,
+	"manaCurrent": 0,
+	"manaMax": 100,
+	"strength": 1,
+	"agility": 1,
+	"magic": 1,
+	"luck": 1,
+	"physicalDefense": 1,
+	"magicResistance": 1
+}
