004e7ffe4994bd75a6ccc088bf03094670b77dbe
only-JB <47716501+only-JB@users.noreply.github.com>
2019-02-18 11:28:10 +0100

    Add files via upload
    Dynamic main menu scene added

---
+++ scripts/MainMenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_ButtonPlay_pressed():
+	pass # TODO: add char creation / char select
+
+func _on_ButtonSettings_pressed():
+	pass # TODO: add settings screen & singleton
+
+func _on_ButtonExit_pressed():
+	get_tree().quit()
