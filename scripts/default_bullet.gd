bed5cadecf59c13a8a40dffc47dac8745db6c842
only-JB <onlyjb93@gmail.com>
2019-02-19 16:57:55 +0100

    added attack/ability generator and bullets

---
+++ scripts/default_bullet.gd
@@ -0,0 +1,45 @@
+extends KinematicBody2D
+
+# The player bullet's collision layer is set to 3:
+# every object (including enemies) that should collide with it, need the same collision layer
+# this will prevent collisions between players and player bullets, unless we define a PVP zone
+
+var origin = Vector2(0,0)
+var direction = Vector2(0,-1)
+var speed = 0
+var max_travel_distance = 0
+
+var damage = 0
+var knockback = 0
+var effects = {}
+
+var gradient = null
+
+
+func _ini(stats, weapon_origin, weapon_rotation): # is called by weapon.gd before the bullet is added to the scene
+	origin = weapon_origin # safe origin to calculate traveled distance
+	position = origin # set position to weapon origin
+	direction = direction.rotated(weapon_rotation) # rotate the bullet according to the weapon node
+	speed = stats.bullet_speed * (rand_range(1-stats.bullet_speed_random,1+stats.bullet_speed_random)) # value to multiply the direction with
+	max_travel_distance = stats.bullet_range * (rand_range(1-stats.bullet_range_random,1+stats.bullet_range_random)) # maximum travel distance in pixel
+	effects = stats.bullet_effects # safe effects of the bullet, ie poison or increased loot
+	
+	damage = stats.damage # set damage to be base damage
+	var damage_range = range(stats.damage_random.x,stats.damage_random.y+1) # declare range of extra damage
+	if len(damage_range) != 0: # if extra damage got atleast 2 different values
+		damage += damage_range[randi()%len(damage_range)] # pick a random value and add it to damage
+	knockback = stats.bullet_knockback * rand_range(1-stats.bullet_knockback_random,1+stats.bullet_knockback_random) # calculate knockback and it's randomness
+	
+	scale = Vector2(stats.bullet_scale,stats.bullet_scale) * rand_range(1-stats.bullet_scale_random,1+stats.bullet_scale_random) # calculate bullet scale and it's randomness
+	if stats.bullet_gradient is Gradient: 								# if a gradient is defined:
+		$MeshInstance2D.modulate = stats.bullet_gradient.get_color(0) 	# 	modulate to first gradient color
+		gradient = stats.bullet_gradient								# 	safe gradient for interpolating colors based on travel distance
+	else:																# if no gradient is defined:
+		$MeshInstance2D.modulate = stats.bullet_color					#	modulate to bullet_color
+	
+func _physics_process(delta):
+	move_and_slide(direction*speed)
+	if gradient != null: # if we use a gradient
+		$MeshInstance2D.modulate = gradient.interpolate(abs((position-origin).length()) / max_travel_distance) # interpolate gradient colors based on travel distance
+	if abs((position-origin).length()) > max_travel_distance: # max travel distance reached? delete yourself!
+		queue_free()
