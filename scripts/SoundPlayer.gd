befe288cf126c8a8ffe1c386e0fac20f20c17d31
Mykolas Peteraitis <mykolas.peteraitis@gmail.com>
2019-02-20 21:57:12 +0200

    Minor updates to SoundPlayer singleton

--- scripts/SoundPlayer.gd
+++ scripts/SoundPlayer.gd
@@ -8,8 +8,8 @@ func play(sound):
 		# Create an AudioStreamPlayer which will play the sound
 		var scene = load("res://scenes/AudioStreamPlayer.tscn")
 		var scene_instance = scene.instance()
-		scene_instance.set_name("sound"+str(count))
 		count += 1
+		scene_instance.set_name("sound"+str(count))
 		add_child(scene_instance)
 		# Specify which sound to play
 		scene_instance.set_stream(sound)
@@ -17,3 +17,5 @@ func play(sound):
 		scene_instance.set_volume_db(8.6858896380650365530225783783321 * log(Global.Sound))
 		# Start playing
 		scene_instance._set_playing(true)
+		return "sound"+str(count)
+	return false
