a1840acfb9e1c12bba47cbf664259c84ebfe1e1a
Mykolas Peteraitis <mykolas.peteraitis@gmail.com>
2019-02-21 14:46:16 +0200

    Add the new button sprites to Pause Menu

--- scripts/PauseButton.gd
+++ scripts/PauseButton.gd
@@ -3,6 +3,7 @@ extends TextureButton
 
 
 func _on_TextureButton_pressed():
+	SoundPlayer.play(load("res://assets/sounds/click.wav"))
 	var scene = load("res://scenes/PauseMenu.tscn")
 	var scene_instance = scene.instance()
 	scene_instance.set_name("PauseMenu")
--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -2,9 +2,11 @@ extends Control
 
 
 func _on_ButtonBack_pressed():
+	SoundPlayer.play(load("res://assets/sounds/click.wav"))
 	queue_free()
 
 
 func _on_ButtonMainMenu_pressed():
+	SoundPlayer.play(load("res://assets/sounds/click.wav"))
 	get_tree().change_scene("res://scenes/MainMenu.tscn")
 	# This shall get more complicated when we set up networking
