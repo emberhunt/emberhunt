77d64f7b701c3c151e093b6ee0a9d835c428dc8c
Mykolas Peteraitis <mykolas.peteraitis@gmail.com>
2019-04-12 19:23:33 +0300

    Bug fixes on the server console

--- server/commands/help.gd
+++ server/commands/help.gd
@@ -1,2 +1,2 @@
 func help(args = []):
-	print("Here's some help:\n\n\tList of commands:\n\t* help")
+	print("Here's some help:\n\n\tList of commands:\n\t* help - display this help page\n\t* listArgs - just lists the arguments")
