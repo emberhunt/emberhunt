6ca9aeb4a6fabe23710bc6a5e65617bc422ef567
Ponas <mykolas.peteraitis@gmail.com>
2019-05-24 19:11:57 +0300

    slight modifications

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -20,109 +20,9 @@ var worldReadyFunctions = {}
 
 var loaded_bullets = {}
 
-const init_stats = {
-	"Knight" : {
-		"max_hp" : 160,
-		"max_mp" : 120,
-		"strength" : 8,
-		"agility" : 4,
-		"magic" : 3,
-		"luck" : 1,
-		"physical_defense" : 18,
-		"magic_defense" : 11 
-	},
-	"Berserker" : {
-		"max_hp" : 120,
-		"max_mp" : 115,
-		"strength" : 15,
-		"agility" : 11,
-		"magic" : 4,
-		"luck" : 1,
-		"physical_defense" : 12,
-		"magic_defense" : 7
-	},
-	"Assassin" : {
-		"max_hp" : 100,
-		"max_mp" : 100,
-		"strength" : 6,
-		"agility" : 14,
-		"magic" : 4,
-		"luck" : 1,
-		"physical_defense" : 7,
-		"magic_defense" : 7 
-	},
-	"Sniper" : {
-		"max_hp" : 80,
-		"max_mp" : 125,
-		"strength" : 9,
-		"agility" : 8,
-		"magic" : 6,
-		"luck" : 1,
-		"physical_defense" : 6,
-		"magic_defense" : 6 
-	},
-	"Hunter" : {
-		"max_hp" : 90,
-		"max_mp" : 115,
-		"strength" : 6,
-		"agility" : 8,
-		"magic" : 5,
-		"luck" : 1,
-		"physical_defense" : 6,
-		"magic_defense" : 6 
-	},
-	"Arsonist" : {
-		"max_hp" : 70,
-		"max_mp" : 200,
-		"strength" : 3,
-		"agility" : 9,
-		"magic" : 13,
-		"luck" : 1,
-		"physical_defense" : 4,
-		"magic_defense" : 16 
-	},
-	"Brand" : {
-		"max_hp" : 70,
-		"max_mp" : 180,
-		"strength" : 4,
-		"agility" : 7,
-		"magic" : 8,
-		"luck" : 1,
-		"physical_defense" : 4,
-		"magic_defense" : 15 
-	},
-	"Herald" : {
-		"max_hp" : 110,
-		"max_mp" : 240,
-		"strength" : 3,
-		"agility" : 10,
-		"magic" : 9,
-		"luck" : 1,
-		"physical_defense" : 5,
-		"magic_defense" : 19 
-	},
-	"Redeemer" : {
-		"max_hp" : 65,
-		"max_mp" : 165,
-		"strength" : 2,
-		"agility" : 10,
-		"magic" : 9,
-		"luck" : 1,
-		"physical_defense" : 5,
-		"magic_defense" : 18 
-	},
-	"Druid" : {
-		"max_hp" : 65,
-		"max_mp" : 170,
-		"strength" : 4,
-		"agility" : 12,
-		"magic" : 11,
-		"luck" : 1,
-		"physical_defense" : 4,
-		"magic_defense" : 18 
-	}
-}
-
+var init_stats = {}
+var items = {}
+var item_types = {}
 
 # game paused
 var paused = false
@@ -153,6 +53,17 @@ func loadGame():
 func _ready():
 	loadGame()
 	_load_bullets()
+	
+	init_stats = load_json_from_file("res://data/class_init_stats.json")
+	items = load_json_from_file("res://data/items.json")
+	item_types = load_json_from_file("res://data/item_types.json")
+
+func load_json_from_file(file_path):
+	var file = File.new()
+	file.open(file_path, file.READ)
+	var text = file.get_as_text()
+	file.close()
+	return JSON.parse(text).result
 
 func spawnPlayerAndGUI(world_name):
 	# Add the player to the world
