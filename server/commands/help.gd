6b44358eb758e0dbf9733e21e69074043f8f74bc
Ponas <mykolas.peteraitis@gmail.com>
2019-06-08 20:42:58 +0300

    bug fix on worlds command

--- server/commands/help.gd
+++ server/commands/help.gd
@@ -1,7 +1,7 @@
 func help(args = [], mainServer = null) -> String:
 	var result = ""
 	if args.size() == 0:
-		result = "Here's some help:\n\n\tList of commands:\n\t* help - display a manual for a specific command\n\t* listargs - just lists the arguments\n\t* fps - gives server FPS (frames per second)\n\t* get_player_data <nickname> - gives the account data with nickname as parameter\n\t* get_account_data <uuid> - gives the account data with UUID as parameter\n\t* worlds [world_name] - gives the world data with optional parameter of the world name"
+		result = "Here's some help:\n\n\tList of commands:\n\t* help - display a manual for a specific command\n\t* listargs - just lists the arguments\n\t* fps - gives server FPS (frames per second)\n\t* get_player_data <nickname> - gives the account data with nickname as parameter\n\t* get_account_data <uuid> - gives the account data with UUID as parameter\n\t* worlds [world_name] [players|bags|enemies|npc] [...] - gives the world data with optional parameters"
 	elif args.size() > 1:
 		result = "help: Too many arguments!"
 	else:
@@ -14,7 +14,7 @@ func help(args = [], mainServer = null) -> String:
 		elif args[0] == "get_account_data":
 			result = "\nget_account_data - gives the account data with UUID as parameter \n\nUSAGE:\tget_account_data player_uuid"
 		elif args[0] == "worlds":
-			result = "\nworlds - gives the world data with optional parameter of the world name \n\nUSAGE:\tworlds [world_name]"
+			result = "\nworlds - gives the world data with optional parameters \n\nUSAGE:\tworlds [world_name] [players|bags|enemies|npc] [...]"
 		else:
 			result = "help: There's no help page for this command ("+args[0]+")"
 	return result
--- server/commands/worlds.gd
+++ server/commands/worlds.gd
@@ -6,8 +6,10 @@ func worlds(args = [], mainServer = null) -> String:
 	else:
 		var worlds = mainServer.worlds.duplicate()
 		for arg in args:
-			if worlds.has(arg):
-				worlds = worlds[arg]
+			if worlds.has(str(arg)):
+				worlds = worlds[str(arg)]
+			elif worlds.has(int(arg)):
+				worlds = worlds[int(arg)]
 			else:
 				return arg+" does not exist"
 		return str(worlds)
