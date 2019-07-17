672d7fc855761b598a2d27f77062deb4ec2adad2
Ponas <mykolas.peteraitis@gmail.com>
2019-07-17 18:26:56 +0300

    update font and update_font.sh script

--- update_font.sh
+++ update_font.sh
@@ -5,22 +5,23 @@
 # Licensed under the GNU General Public License v3.0 or later
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-REMOTE_COMMITS=$(curl -s "https://api.github.com/repos/emberhunt/emberfont/commits" | jq length)
-REMOTE_COMMITS=$(($REMOTE_COMMITS+0))
-LOCAL_COMMITS=$(cat ./assets/emberfont/version.txt)
-LOCAL_COMMITS=$(($LOCAL_COMMITS+0))
+echo "Fetching the latest commit info from GitHub."
 
-if [ "$REMOTE_COMMITS" -gt "$LOCAL_COMMITS" ] ; then
+REMOTE_COMMITS=$(curl -s "https://api.github.com/repos/emberhunt/emberfont/commits")
+REMOTE_COMMITS=$(printf "$REMOTE_COMMITS")
+LOCAL_COMMITS=$(cat ./assets/emberfont/version.txt)
+
+if [ "$REMOTE_COMMITS" = "$LOCAL_COMMITS" ] ; then
+	echo "The font is already up to date."
+else
 	echo "Updating..."
-	if (wget -nv --no-check-certificate --content-disposition https://github.com/emberhunt/emberfont/raw/master/emberfont.ttf -O assets/emberfont/temp_emberfont.ttf) ; then
+	if (wget -nv --no-check-certificate --content-disposition https://github.com/emberhunt/emberfont/raw/master/emberfont.ttf -O assets/emberfont/emberfont.ttf.part) ; then
 		echo "Download complete."
 		rm assets/emberfont/emberfont.ttf
-		mv assets/emberfont/temp_emberfont.ttf assets/emberfont/emberfont.ttf
-		printf $REMOTE_COMMITS > assets/emberfont/version.txt
+		mv assets/emberfont/emberfont.ttf.part assets/emberfont/emberfont.ttf
+		printf "$REMOTE_COMMITS" > assets/emberfont/version.txt
 		echo "Update successful."
 	else
 		echo "Font download failed."
 	fi
-else
-	echo "The font is already up to date."
 fi
