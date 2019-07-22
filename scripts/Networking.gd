7f5abc08938a13800eb0473920769d9832743449
Ponas <mykolas.peteraitis@gmail.com>
2019-07-22 21:15:02 +0300

    update server ip

--- scripts/Networking.gd
+++ scripts/Networking.gd
@@ -7,7 +7,7 @@ extends Node
 
 # This is the CLIENT's side of networking
 
-const SERVER_IP = "cnidarias.net"
+const SERVER_IP = "emberhunt.cnidarias.net"
 const SERVER_PORT = 22122
 
 var connected = false
